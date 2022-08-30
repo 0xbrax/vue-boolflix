19f10ab734c8eea6fb70de77af8ba0618e2fa266
Marco Braccini <marco.braccini@live.it>
2022-08-30 17:23:04 +0200

    Milestone 1 OK

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <TopBar @searchInput="getSearchInput" />
-    <MainSection :searchedInput="linkedSearchInput" />
+    <MainSection :linkedMoviesList="moviesList" />
   </div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue'
 import MainSection from '@/components/MainSection.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -17,12 +19,30 @@ export default {
   },
   data() {
     return {
-      linkedSearchInput: ''
+      linkedSearchInput: '',
+      moviesList: []
     }
   },
   methods: {
     getSearchInput(givenSearchInput) {
       this.linkedSearchInput = givenSearchInput;
+      if (!this.linkedSearchInput == '') {
+        this.moviesListRequest();
+      } else {
+        this.moviesList = [];
+      }
+    },
+    moviesListRequest() {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a2e57431dfc8a0bdbcd28a2ec61e3e89&query=${this.linkedSearchInput}&language=it-IT`)
+      .then(response => {
+        //console.log(response);
+        this.moviesList = response.data.results;
+
+        console.log(this.moviesList);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
   }
 }
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,51 +1,21 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="(movie, movieIndex) in getMoviesList" :key="movieIndex">
-                {{movie.title}}
-            </li>
-        </ul>
-    </div>
+    <main>
+        <section v-if="linkedMoviesList.length !== 0">
+            <div v-for="(movie, movieIndex) in linkedMoviesList" :key="movieIndex">
+                <h3>{{movie.title}}</h3>
+                <div>{{movie.original_title}}</div>
+                <div>{{movie.original_language}}</div>
+                <div>{{movie.vote_average}}</div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
     name: 'MainSection',
     props: {
-        searchedInput: String
-    },
-    data() {
-        return {
-            moviesList: []
-        }
-    },
-    methods: {
-        moviesListRequest() {
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a2e57431dfc8a0bdbcd28a2ec61e3e89&query=${this.searchedInput}&language=it-IT`)
-            .then(response => {
-                //console.log(response);
-                this.moviesList = response.data.results;
-
-                console.log(this.moviesList)
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
-    },
-    computed: {
-        getMoviesList() {
-            const filteredMoviesList = [];
-
-            if (this.searchedInput == '') {
-                return filteredMoviesList
-            } else {
-                this.moviesListRequest();
-                return this.moviesList
-            }
-        }
+        linkedMoviesList: Array
     }
 }
 </script>
